d4513e7be0194ebbf37e898ac7ec896d6177498c
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2024-12-04 23:59:33 +0100

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 // *** CAROUSEL ***
 // TODO
-carousel=document.querySelector(".carousel__container")
-carousel.scrollLeft=520;
-carousel.scrollBy({left: +260, behavior:"smooth"});
+const carousel = document.querySelector(".carouselcontainer");
+const prevButton = document.querySelector(".carouselbutton--prev");
+const nextButton = document.querySelector(".carouselbutton--next");
+const premierItem = document.querySelector(".carouselitem");
+
+if (carousel) {
+  prevButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
+  });
+
+  nextButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
+    carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
+  });
+}
